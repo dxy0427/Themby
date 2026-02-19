d83d27bc202257e75230964674bda9cb36d79703
dxy <137971972+dxy0427@users.noreply.github.com>
2026-02-19 18:21:45 +0800

    更新 item_bars.dart

--- lib/src/features/emby/presentation/emby_library/item_bars.dart
+++ lib/src/features/emby/presentation/emby_library/item_bars.dart
@@ -2,10 +2,10 @@
 import 'package:themby/src/features/emby/domain/tab_model.dart';
 
 final List<TabModel> tabs = [
-  TabModel('all', '0', 'all', false),
-  TabModel('recent', '1', 'recent', false),
-  TabModel('collection', '2', 'collection', false),
-  TabModel('genre', '3', 'genre', false),
-  TabModel('tag', '4', 'tag', false),
-  TabModel('favorite', '5', 'favorite', false),
+  TabModel('all', '0', '全部', false),
+  TabModel('recent', '1', '继续播放', false),
+  TabModel('collection', '2', '合集', false),
+  TabModel('genre', '3', '类型', false),
+  TabModel('tag', '4', '标签', false),
+  TabModel('favorite', '5', '收藏', false),
 ];
